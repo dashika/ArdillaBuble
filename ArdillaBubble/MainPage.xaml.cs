90bc62cc2147fe48e489e549439c70fbceb5f439
dashika <belyaevadasha@bk.ru>
2017-05-03 13:39:15 +0300

    old student project

---
+++ ArdillaBubble/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ArdillaBubble
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LayoutRoot_Loaded(object sender, RoutedEventArgs e)
+        {
+            Load.Begin();
+        }
+
+        /// <summary>
+        /// После перехода в рамке (Frame) проверьте, что ссылка <see cref="HyperlinkButton"/> представляет текущую выбранную страницу
+        /// </summary>
+        private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            foreach (UIElement child in LinksStackPanel.Children)
+            {
+                HyperlinkButton hb = child as HyperlinkButton;
+                if (hb != null && hb.NavigateUri != null)
+                {
+                    if (hb.NavigateUri.ToString().Equals(e.Uri.ToString()))
+                    {
+                        VisualStateManager.GoToState(hb, "ActiveLink", true);
+                    }
+                    else
+                    {
+                        VisualStateManager.GoToState(hb, "InactiveLink", true);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Если при переходе возникла ошибка, отобразить окно сообщения
+        /// </summary>
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show(e.Exception.ToString());
+        }
+    }
+
+
+}
---
+++ ArdillaBubble/Views/Photo.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+using System.Windows.Media.Imaging;
+
+namespace ArdillaBubble.Views
+{
+    public partial class Photo : Page
+    {
+        public Photo()
+        {
+            InitializeComponent();
+
+         //   Media.Source = new BitmapImage(new Uri("1.jpg", UriKind.Relative));
+
+            Image im = new Image();
+            im.Source = new BitmapImage(new Uri("1.jpg", UriKind.Relative));
+            LayoutRoot.Children.Add(im);
+
+        }
+
+        // Выполняется, когда пользователь переходит на эту страницу.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+   }
+
+    }
+}
